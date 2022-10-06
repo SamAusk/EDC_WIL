5574dea57ecebc1ba48e795bf5d7a260d2646907
SamAusk <krittawat.auskulsuthi@griffithuni.edu.au>
2022-10-06 10:47:52 +1000

    Add variable

--- config.php
+++ config.php
@@ -1,5 +1,9 @@
 <?php 
-    $con = mysqli_connect('localhost','sam','test1234','creative cloud');
+    $hostname = "localhost";
+    $username = "sam";
+    $password = "test1234"; 
+    $database = "Creative_database1";
+    $con = mysqli_connect( $hostname,$username,$password,$database);
     if (!$con) {
         die("Connection failed: " . mysqli_connect_error());
       }
